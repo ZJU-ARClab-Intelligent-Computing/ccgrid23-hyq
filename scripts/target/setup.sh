a4fb1ab1d2430e606c411aa820614a34bc995757
Jinlong Chen <nickyc975@zju.edu.cn>
2023-02-02 19:15:51 +0800

    Fix modprobe module parameters

--- scripts/target/setup.sh
+++ scripts/target/setup.sh
@@ -28,27 +28,21 @@ if [ ! -e "${MODULEDIR}/configure.mk.kernel" ]; then
                 --with-nvmf_host-mod \
                 --with-nvmf_target-mod \
                 -j40 && \
-    make -j40 && \
-    sudo make install -j40
-fi
-
-if [ $(/usr/sbin/lsmod | grep -w nvmet_rdma | wc -l) -eq 0 ]; then
-    sudo /usr/sbin/rmmod nvmet nvme nvme-core
-    sudo /usr/sbin/modprobe nvme num_p2p_queues=$NUM_P2P_QUEUES_PER_DEV
-    sudo /usr/sbin/modprobe nvmet-rdma
 fi
 
 sudo ${SCRIPTDIR}/config_subsystems.sh stop
-sudo /usr/sbin/rmmod nvmet-rdma
 
 cd ${MODULEDIR} && git checkout $1
 cd ${MODULEDIR} && make -j40 && sudo make install -j40
 
+# Reload MLNX drivers.
 sudo /etc/init.d/openibd force-restart
 
 # Wait for the network ready.
 sleep 5
 
+sudo /usr/sbin/rmmod nvmet-rdma nvmet nvme nvme-core
+sudo /usr/sbin/modprobe nvme num_p2p_queues=$NUM_P2P_QUEUES_PER_DEV
 sudo /usr/sbin/modprobe nvmet-rdma
 
 sudo ${SCRIPTDIR}/config_subsystems.sh start $3 $2 4420 $NUM_P2P_QUEUES_PER_DEV
